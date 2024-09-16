a844b9e6e40e0aa810dc11cbbe9b07cf1483004d
qurban <qurbanmqm@gmail.com>
2024-09-16 22:29:14 +0400

    changes

--- src/Todolist.js
+++ src/Todolist.js
@@ -2,7 +2,11 @@ import React, { useState } from "react";
 
 function Todolist() {
   const [tasks, setTasks] = useState([]);
-  const [task, setTask] = useState({ text: "", index: null });
+  const [task, setTask] = useState({
+    text: "",
+    index: null,
+    isCompleted: false,
+  });
 
   const handleChange = (e) => {
     setTask({ ...task, text: e.target.value });
@@ -12,20 +16,26 @@ function Todolist() {
     if (task.text.trim() === "") return;
 
     if (task.index === null) {
-      // Добавление новой задачи
-      setTasks([...tasks, task.text]);
+      setTasks([...tasks, { text: task.text, isCompleted: false }]);
     } else {
       const updatedTasks = tasks.map((t, i) =>
-        i === task.index ? task.text : t
+        i === task.index ? { ...t, text: task.text } : t
       );
       setTasks(updatedTasks);
     }
 
-    setTask({ text: "", index: null });
+    setTask({ text: "", index: null, isCompleted: false });
   };
 
   const startEdit = (index) => {
-    setTask({ text: tasks[index], index });
+    setTask({ text: tasks[index].text, index });
+  };
+
+  const startCompleted = (index) => {
+    const updatedTasks = tasks.map((t, i) =>
+      i === index ? { ...t, isCompleted: !t.isCompleted } : t
+    );
+    setTasks(updatedTasks);
   };
 
   const removeTask = (index) => {
@@ -48,16 +58,27 @@ function Todolist() {
         </button>
       </div>
       <ol className="todo-list">
-        {tasks.map((taskText, index) => (
-          <li key={index}>
-            <span className="text">{taskText}</span>
-            <button className="delete" onClick={() => removeTask(index)}>
-              Remove task
-            </button>
+        {tasks.map((taskObj, index) => (
+          <li
+            key={index}
+            className={taskObj.isCompleted ? "completed-task" : ""}
+          >
+            <span className="text">{taskObj.text}</span>
+            <div className="btn-wrapper">
+              <button className="delete" onClick={() => removeTask(index)}>
+                Remove task
+              </button>
 
-            <button className="change" onClick={() => startEdit(index)}>
-              Change task
-            </button>
+              <button className="change" onClick={() => startEdit(index)}>
+                Change task
+              </button>
+              <button
+                className="completed"
+                onClick={() => startCompleted(index)}
+              >
+                {taskObj.isCompleted ? "Uncomplete task" : "Complete task"}
+              </button>
+            </div>
           </li>
         ))}
       </ol>
